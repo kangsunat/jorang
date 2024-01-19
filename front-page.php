4c96ea9b803c675e158caa0566b05effdfba977b
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-01-19 15:34:37 +0700

    update

---
+++ front-page.php
@@ -0,0 +1,4 @@
+<?php get_header();?>
+
+front page
+<?php get_footer();?>
--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JORANG</title>
-    <?php wp_head(  ) ?>
+    <?php wp_head(); ?>
 </head>
 <body>
     
