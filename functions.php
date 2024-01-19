cc378f974bf2fbb44c5050421fd6d95f6ec367e2
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-01-19 16:06:58 +0700

    dinamic title tag and remove some folder

--- functions.php
+++ functions.php
@@ -1,7 +1,15 @@
 <?php
 
+function theme_support() {
+    // diamic title tag
+    add_theme_support( 'title-tag' );
+}
+
+add_action('after_setup_theme','theme_support');
+
 function load_css() {
-    wp_enqueue_style( 'tailwindcss', get_template_directory_uri(  ).'/style.css',array(), '1.0', 'all' );
+    $version = wp_get_theme()->get("Version");
+    wp_enqueue_style( 'tailwindcss', get_template_directory_uri(  ).'/style.css',array(), $version, 'all' );
 }
 
 add_action('wp_enqueue_scripts','load_css');
