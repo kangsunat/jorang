81393d845a389c8d56fd3cc0ca3bf1e2e775fbbc
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-01-21 00:33:45 +0700

    update

--- front-page.php
+++ front-page.php
@@ -1,4 +1,14 @@
-<?php get_header();?>
+<?php get_header(); ?>
 
-front page
-<?php get_footer();?>
+<header>
+
+    <video style="margin: 0; padding: 0;" class="w-full h-screen bg-cover aspect-video" controls>
+        <source
+            src="http://jorang.test/wp-content/uploads/2024/01/Best-FREE-AI-Text-to-Animated-Video-Generator-Disney-Animation-Inspired-11ai-YouTube.mp4"
+            type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+
+</header>
+
+<?php get_footer(); ?>
