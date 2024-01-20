bf22e63757dc071372c6260d8facf11a4fd77b6e
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-01-21 00:00:07 +0700

    template parts navbar

---
+++ template-parts/navbar.php
@@ -0,0 +1,46 @@
+<!-- menus -->
+<nav>
+    <div class="container nav">
+
+        <!-- company logo -->
+        <div class="company-logo">
+
+            <?php
+            if (function_exists("the_custom_logo")) {
+                $custom_logo_id = get_theme_mod('custom_logo');
+                $logo = wp_get_attachment_image_src($custom_logo_id);
+
+            }
+            ;
+            ?>
+            <img src="<?= $logo[0] ?>" alt="company-logo">
+            <p>
+                <?= get_bloginfo('name') ?>
+            </p>
+        </div>
+        <!-- end company logo -->
+
+        <div class='flex items-center gap-8'>
+            <!-- menus -->
+            <?php
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'head-menus',
+                    'menu_class' => 'head-menus',
+                    'container' => 'div',
+                    'container_class' => '',
+                )
+            );
+            ?>
+            <!-- end menus -->
+
+            <!-- sosmed -->
+            <ul class="sosmed flex items-center gap-3">
+                <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+            </ul>
+            <!-- end sosmed -->
+        </div>
+    </div>
+</nav>
