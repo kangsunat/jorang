646948ae97db38bdd816397d2cb39316e7e37470
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-01-24 14:10:12 +0700

    cta

--- front-page.php
+++ front-page.php
@@ -2,4 +2,5 @@
 <?php get_template_part('template-parts/home/header') ?>
 <?php get_template_part('template-parts/home/about') ?>
 <?php get_template_part('template-parts/home/services') ?>
+<?php get_template_part('template-parts/home/cta') ?>
 <?php get_footer(); ?>
