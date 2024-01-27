c88df32d65f20bb6b2e49119a25ced2dfd109b45
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-01-27 10:11:54 +0700

    portfolio cta

--- page-portfolio.php
+++ page-portfolio.php
@@ -3,6 +3,8 @@
 <main class="container">
     <?php get_template_part('template-parts/portfolio/header') ?>
     <?php get_template_part('template-parts/portfolio/portfolio') ?>
+    <?php get_template_part('template-parts/portfolio/our-client') ?>
+    <?php get_template_part('template-parts/portfolio/cta') ?>
 </main>
 
 <?php get_footer(); ?>
---
+++ template-parts/portfolio/cta.php
@@ -0,0 +1,6 @@
+<section class="px-20 py-10 flex justify-between items-center">
+    <h2 class="text-6xl font-semibold">Let's work <br> together</h2>
+    <a href="#">
+        <i class="fa-solid fa-arrow-right text-2xl"></i>
+    </a>
+</section>
