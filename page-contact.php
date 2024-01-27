428f5849ad0f2a9a52eb7c24052273f8fb3b55b9
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-01-27 11:37:36 +0700

    address

--- page-contact.php
+++ page-contact.php
@@ -3,6 +3,7 @@
 <main class="bg-black text-white">
     <?php get_template_part('template-parts/contact/header') ?>
     <?php get_template_part('template-parts/contact/form') ?>
+    <?php get_template_part('template-parts/contact/address') ?>
 </main>
 
 <?php get_footer(); ?>
---
+++ template-parts/contact/address.php
@@ -0,0 +1,27 @@
+<section class="bg-white text-black py-20">
+    <div class="container">
+        <div class="flex items-center gap-8 pl-10">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.1628753423142!2d110.41965527513591!3d-6.990088293010945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708b592bbd8ed7%3A0xddbb6b6596ae4a35!2sJl.%20Simpang%20Lima%2C%20Kota%20Semarang%2C%20Jawa%20Tengah!5e0!3m2!1sen!2sid!4v1706329386131!5m2!1sen!2sid"
+                width="500" style="border:0;" allowfullscreen="" loading="lazy" class="aspect-video"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div class="space-y-6">
+                <p class="font-medium text-lg mb-10">Semarang City, Central Java</p>
+                <div class="flex items-center gap-3">
+                    <i class="fa-solid fa-location-pin"></i>
+                    <p class="text-neutral-600">Jl. Simpang Lima</p>
+                </div>
+                <div class="flex items-center gap-3">
+                    <i class="fa-solid fa-envelope"></i>
+                    <p class="text-neutral-600">sample_email@email.com</p>
+                </div>
+                <div class="flex items-center gap-3">
+                    <i class="fa-solid fa-phone"></i>
+                    <a href=" https://wa.me/628888888888?text='sample message here'" class="text-neutral-600">+62 8xx
+                        xxxx
+                        xxxx</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
