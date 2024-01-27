d39d2e262bcc38c4f25d2a05ff7250fd95567bc1
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-01-27 09:53:30 +0700

    portfolio page

--- page-portfolio.php
+++ page-portfolio.php
@@ -2,6 +2,7 @@
 
 <main class="container">
     <?php get_template_part('template-parts/portfolio/header') ?>
+    <?php get_template_part('template-parts/portfolio/portfolio') ?>
 </main>
 
 <?php get_footer(); ?>
--- template-parts/portfolio/header.php
+++ template-parts/portfolio/header.php
@@ -1,3 +1,3 @@
-<header>
+<header class="border-b-2 border-black">
     <h2 class="text-6xl font-semibold text-center py-14">Our projects</h2>
 </header>
