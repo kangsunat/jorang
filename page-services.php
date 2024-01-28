f88f572d34947e034300fc21c51b601418406433
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-01-28 21:47:36 +0700

    services

--- page-services.php
+++ page-services.php
@@ -1,7 +1,8 @@
 <?php get_header(); ?>
 
-<main>
+<main class="space-y-10">
     <?php get_template_part('template-parts/services/header') ?>
+    <?php get_template_part('template-parts/services/services') ?>
 </main>
 
 <?php get_footer(); ?>
---
+++ template-parts/services/services.php
@@ -0,0 +1,20 @@
+<section class="flex flex-col gap-8 container">
+    <div class="grid grid-cols-2 gap-6 place-items-center">
+        <img src="https://picsum.photos/200" class="aspect-square w-full" alt="">
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ullam amet molestias? Commodi quod voluptatibus
+            autem iste iure facilis ut, deserunt magnam laboriosam suscipit odio dolore, quibusdam, id quis repellat.
+        </div>
+    </div>
+    <div class="grid grid-cols-2 gap-6 place-items-center">
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ullam amet molestias? Commodi quod voluptatibus
+            autem iste iure facilis ut, deserunt magnam laboriosam suscipit odio dolore, quibusdam, id quis repellat.
+        </div>
+        <img src="https://picsum.photos/200" class="aspect-square w-full" alt="">
+    </div>
+    <div class="grid grid-cols-2 gap-6 place-items-center">
+        <img src="https://picsum.photos/200" class="aspect-square w-full" alt="">
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ullam amet molestias? Commodi quod voluptatibus
+            autem iste iure facilis ut, deserunt magnam laboriosam suscipit odio dolore, quibusdam, id quis repellat.
+        </div>
+    </div>
+</section>
